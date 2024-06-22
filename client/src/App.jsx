49e0babab43eab7860d9285f2388991b6555d3b7
AtirujRat <136755600+RattRuj@users.noreply.github.com>
2024-06-22 23:50:43 +0700

    init project]

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,6 @@ import "./App.css";
 import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
 import TouristAttractionList from "./components/TouristAttractionList";
-
 import axios from "axios";
 
 function App() {
